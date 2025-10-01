592ed64247a9ed82c9dae8929ee7592b433b3115
YassineMATROUF <ymatrou94@gmail.com>
2025-10-01 09:18:29 +0200

    Ajout du fichier Add  en JS

---
+++ Addition.js
@@ -0,0 +1,10 @@
+// Demander deux nombres à l'utilisateur
+let nombre1 = parseFloat(prompt("Entrez le premier nombre :"));
+let nombre2 = parseFloat(prompt("Entrez le deuxième nombre :"));
+
+// Addition
+let resultat = nombre1 + nombre2;
+
+// Afficher le résultat
+alert("La somme est : " + resultat);
+console.log("La somme est :", resultat);
